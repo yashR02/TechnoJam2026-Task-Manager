06775ed28301a3ce69e14293664fb8d9c2f3c0ba
yashR02 <yashambarbro0218@gmail.com>
2026-09-06 18:35:56 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,10 @@ mongoose
   .then(() => {
     console.log("MongoDB connected successfully");
 
-    app.listen(5000, () => {
-      console.log("Server running on http://localhost:5000");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
